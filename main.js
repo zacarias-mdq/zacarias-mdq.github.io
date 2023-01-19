4188fbd3ef6a5250094adfb80961b625f33b6dd1
zacarias-mdq <109445698+zacarias-mdq@users.noreply.github.com>
2023-01-18 22:56:16 -0300

    Update main.js
    corregi el destino de la imagen 1

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let imagen = document.getElementById("fotoperfil");
 
 imagen.onclick = function () {
     console.log(imagen)    ;
-    if (imagen.src==("http://127.0.0.1:5500/img/perfil.png")){
+    if (imagen.src==("img/perfil.png")){
         imagen.src = "img/perfil2.png";}
         else{
             imagen.src = "img/perfil.png";
